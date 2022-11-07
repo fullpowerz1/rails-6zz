cb6ad3e780b6c5dcb34d7b30074c9d1ecc9ebd20
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2022-11-07 10:36:36 +0000

    hensuu

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:index,:show,:edit,:update]
   get "home/about"=>"homes#about"
 end
